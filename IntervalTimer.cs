af39920bd0af6937f5a67b2913845196cd8c93b3
Jerome Cheng <jcheng31@gmail.com>
2013-06-01 22:56:48 +0800

    Initial commit. Basic IntervalTimer constructors.

---
+++ IntervalTimer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.SPOT;
+
+namespace IntervalTimer
+{
+    public class IntervalTimer
+    {
+        public TimeSpan ShortDuration { get; set; }
+        public TimeSpan LongDuration { get; set; }
+
+        public IntervalTimer(TimeSpan shortDuration, TimeSpan longDuration)
+        {
+            ShortDuration = shortDuration;
+            LongDuration = longDuration;
+        }
+
+        public IntervalTimer(int shortSeconds, int longSeconds)
+        {
+            ShortDuration = new TimeSpan(0, 0, shortSeconds);
+            LongDuration = new TimeSpan(0, 0, longSeconds);
+        }
+    }
+}
