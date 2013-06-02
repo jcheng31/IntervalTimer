2fa2e788fdb7372c14c2c2e5c5ea4919d7528dc7
Jerome Cheng <jcheng31@gmail.com>
2013-06-02 12:05:59 +0800

    Basic implementation of timer.

--- IntervalTimer.cs
+++ IntervalTimer.cs
@@ -12,6 +12,8 @@ namespace IntervalTimer
         public TimeSpan ShortDuration { get; set; }
         public TimeSpan LongDuration { get; set; }
 
+        private ExtendedTimer _timer;
+
         public IntervalTimer(TimeSpan shortDuration, TimeSpan longDuration)
         {
             ShortDuration = shortDuration;
@@ -24,6 +26,46 @@ namespace IntervalTimer
             LongDuration = new TimeSpan(0, 0, longSeconds);
         }
 
-        
+        public void Start()
+        {
+            _timer = new ExtendedTimer(InternalTimerTriggered, true, TimeSpan.Zero, ShortDuration);
+        }
+
+        private void InternalTimerTriggered(object state)
+        {
+            bool isLastDurationShort = (bool)state;
+
+            TimeSpan nextDuration;
+            String eventMessage;
+            IntervalType previousDurationType;
+
+            if (isLastDurationShort)
+            {
+                nextDuration = LongDuration;
+                eventMessage = "Short Duration Triggered";
+                previousDurationType = IntervalType.Short;
+            }
+            else
+            {
+                nextDuration = ShortDuration;
+                eventMessage = "Long Duration Triggered";
+                previousDurationType = IntervalType.Long;
+            }
+
+            _timer.Dispose();
+            _timer = new ExtendedTimer(InternalTimerTriggered, !isLastDurationShort, TimeSpan.Zero, nextDuration);
+            
+            IntervalReachedEventArgs arguments = new IntervalReachedEventArgs(previousDurationType, eventMessage);
+            OnIntervalCompleted(arguments);
+        }
+
+        private void OnIntervalCompleted(IntervalReachedEventArgs arguments)
+        {
+            IntervalEventHandler handler = IntervalCompleted;
+            if (handler != null)
+            {
+                handler(this, arguments);
+            }
+        }
     }
 }
