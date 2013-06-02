09628d92fa3f2dced789f48026203751dfc73f93
Jerome Cheng <jcheng31@gmail.com>
2013-06-02 11:22:18 +0800

    Add IntervalReachedEventArgs class and definition.

---
+++ IntervalReachedEventArgs.cs
@@ -0,0 +1,23 @@
+using System;
+using Microsoft.SPOT;
+
+namespace IntervalTimer
+{
+    public enum IntervalType
+    {
+        Short,
+        Long
+    };
+    
+    class IntervalReachedEventArgs : EventArgs
+    {
+        public IntervalType Type { get; set; }
+        public String Message { get; set; }
+
+        public IntervalReachedEventArgs(IntervalType type, String message)
+        {
+            Type = type;
+            Message = message;
+        }
+    }
+}
