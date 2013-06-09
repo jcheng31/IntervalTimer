a3f685419fe2330782272ccf2720b27750390346
Jerome Cheng <jcheng31@gmail.com>
2013-06-09 11:31:18 +0800

    Remove IntervalType from IntervalReachedEventArgs.

--- IntervalReachedEventArgs.cs
+++ IntervalReachedEventArgs.cs
@@ -3,20 +3,12 @@ using Microsoft.SPOT;
 
 namespace IntervalTimer
 {
-    public enum IntervalType
-    {
-        Short,
-        Long
-    };
-
     public class IntervalReachedEventArgs : EventArgs
     {
-        public IntervalType Type { get; set; }
         public String Message { get; set; }
 
-        public IntervalReachedEventArgs(IntervalType type, String message)
+        public IntervalReachedEventArgs(String message)
         {
-            Type = type;
             Message = message;
         }
     }
